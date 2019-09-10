d9e5328e62b7f89a8e265f612b5e1d134b781b0f
b-e-d-o <36853269+b-e-d-o@users.noreply.github.com>
2019-09-10 19:32:33 +0200

    biuld admin's things

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Book, BookInstance, Author, Genre, Language
 
-# Register your models here.
+ 
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'language')
+    inlines = [BookInstanceInline]
+    
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status',)
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        ('Book Info',{
+            'fields':('book', 'id')
+            }),
+        ('Avilability',{'fields':('status', 'due_back'),
+                        }),
+                )
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    
+admin.site.register(Genre)
+admin.site.register(Language)
