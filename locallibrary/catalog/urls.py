d5bea676620628e83d19e6b377a3c57ac35cd92b
b-e-d-o <36853269+b-e-d-o@users.noreply.github.com>
2019-09-14 19:48:31 +0200

    adding the home page

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from catalog import views
 urlpatterns = [
-    
+    path('', views.index, name='index'),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from catalog.models import Book, BookInstance, Author
 
-# Create your views here.
+def index(request):
+    num_books = Book.objects.count()
+    num_book_instances = BookInstance.objects.count()
+    num_avilable_book_instances = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    
+    context = {
+        'num_books' : num_books,
+        'num_book_instances' : num_book_instances,
+        'num_avilable_book_instances' : num_avilable_book_instances,
+        'num_authors' : num_authors,
+    }
+    return render(request, 'index.html', context=context)
